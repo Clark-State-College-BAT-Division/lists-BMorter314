ccdddd81993e802b785260dbd2c2bed53c0dbd8f
Brennen Morter <morteb2348@clarkstate.edu>
2026-03-22 20:59:01 -0400

    Update meanmedian.py

--- meanmedian.py
+++ meanmedian.py
@@ -4,3 +4,23 @@
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
 # video url: https://youtu.be/VGrRdUunjXg
+
+numbers = []
+for i in range(5):
+    num = float(input(f"Enter number {i+1}: "))
+    numbers.append(num)
+print("\nOrignal list:", numbers)
+print("Small to large:", sorted(numbers))
+print("Large to small:", sorted(numbers, reverse=True))
+
+mean = sum(numbers) / len(numbers)
+print("Mean:", mean)
+
+nums = sorted(numbers)
+mid = len(nums) // 2
+
+if len(nums) % 2 == 1:
+    median = nums[mid]
+else:
+    median = (nums[mid - 1] + nums[mid]) / 2
+print("Median:", median)
